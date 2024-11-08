168a69e553002ea9b2b7918425967a3488e24476
Troy <tanaka_s2@yahoo.com>
2024-11-07 23:40:47 -0600

    Stats DB skeleton flow

--- scripts/Game/Core/SCR_BaseGameMode.c
+++ scripts/Game/Core/SCR_BaseGameMode.c
@@ -1,9 +1,29 @@
 modded class SCR_BaseGameMode
 {
+	// SERVER METHODS
+	override void OnGameStart()
+	{
+		super.OnGameStart();
+		
+		// Setup and connect to the db
+		
+	}
+	
 	override void OnPlayerConnected(int playerId)
 	{
 		super.OnPlayerConnected(playerId);
 		
-		// Check if player is registered 
+		// Verify user has account and if not, tell them to register for stats
+		
+	}
+	
+	// GLOBAL METHODS 
+	override void OnGameModeStart()
+	{	
+		super.OnGameModeStart();
+		if (Replication.IsClient) return;
+		
+		// Create mission entry if players > 10 (QA testing)
+		
 	}
 }
