22ac470ee48cc5b381d2e23b090f8d6db37a4354
Njpatman <doomday1224@gmail.com>
2025-01-15 08:53:08 -0500

    Adds Respawn Poles and Teleport Pole System

---
+++ scripts/Game/Systems/Respawns/CRF_RespawnPointComponent.c
@@ -0,0 +1,33 @@
+class CRF_RespawnPointComponentClass: ScriptComponentClass {}
+
+class CRF_RespawnPointComponent: ScriptComponent
+{
+	[Attribute("true", "auto", "")]
+	bool m_bActiveRespawnPoint;
+	
+	[Attribute("", uiwidget: UIWidgets.ComboBox, enums: {ParamEnum("", ""), ParamEnum("BLUFOR", "BLUFOR"), ParamEnum("OPFOR", "OPFOR"), ParamEnum("INDFOR", "INDFOR"), ParamEnum("CIV", "CIV")})]
+	string m_sRespawnPointFaction;
+	
+	[Attribute("1", "auto", "")]
+	int m_iRespawnPointPriority;
+	
+	//---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+	override void OnPostInit(IEntity owner)
+	{
+		super.OnPostInit(owner);
+		
+		if (!GetGame().InPlayMode() || !Replication.IsServer()) return;
+		
+		CRF_Gamemode.GetInstance().RegisterRespawnPoint(owner);
+	};
+	
+	void SetRespawnPointPriority(int priority)
+	{
+		m_iRespawnPointPriority = priority;
+	}
+	
+	void SetRespawnActiveState(bool active)
+	{
+		m_bActiveRespawnPoint = active;
+	}
+};
