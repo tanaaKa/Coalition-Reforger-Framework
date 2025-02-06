ea94ebf8e2954c2cbb637838ee7815ef4ef17fd0
Njpatman <doomday1224@gmail.com>
2025-02-05 22:51:32 -0500

    Fixed Compass and Icon Changes

--- scripts/Game/Systems/UI/Spectator/CRF_SpectatorLabelIcon.c
+++ scripts/Game/Systems/UI/Spectator/CRF_SpectatorLabelIcon.c
@@ -10,14 +10,14 @@ class CRF_SpectatorLabelIcon : SCR_ScriptedWidgetComponent
 	
 	protected float m_fMaxIconDistance = 800.0;
 	protected float m_fMinIconDistance = 10.0;
-	[Attribute("64.0")]
+	[Attribute("42.0")]
 	protected float m_fMaxIconSize;
 	[Attribute("4.0")]
 	protected float m_fMinIconSize;
 	protected float m_fMaxIconOpacity = 1;
 	protected float m_fMinIconOpacity = 0.8;
 	
-	protected float m_fMaxLabelDistance = 25.0;
+	protected float m_fMaxLabelDistance = 30.0;
 	protected float m_fMinLabelDistance = 10.0;
 	
 	protected TNodeId w_iBoneIndex;
